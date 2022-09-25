f786158f2b264c2dba9cbe4fb3f1827e2b6d0294
abigailcfreeman <abigailcfreemandev@gmail.com>
2022-09-25 23:47:32 +0200

    stats

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,8 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ feedback, handleDelete }) {
 
 if(!feedback || feedback.length === 0){
     return <p>No feedback yet</p>
@@ -8,10 +10,26 @@ if(!feedback || feedback.length === 0){
   return (
     <div className='feedback-list'>
         {feedback.map((item)=>(
-        <FeedbackItem key={item.id} item={item}/>
-        ))}
-        </div>
+        <FeedbackItem 
+        key={item.id}
+        item={item}
+        handleDelete={handleDelete}
+        />
+      ))}
+    </div>
   )
 }
+
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  )
+}
+
 
 export default FeedbackList
